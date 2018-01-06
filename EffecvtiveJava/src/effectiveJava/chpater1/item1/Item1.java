dd620c890c4ed01d23d84dfea02375bb66f8414e
hadj naasse abdeldjalil <hn.abdeldjalil@gmail.com>
2018-01-06 17:27:32 +0100

    Github setting up

--- EffecvtiveJava/src/effectiveJava/chpater1/item1/Item1.java
+++ EffecvtiveJava/src/effectiveJava/chpater1/item1/Item1.java
@@ -3,5 +3,6 @@ package effectiveJava.chpater1.item1;
 public class Item1 {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println(new Object() == new Object());
 	}
 }
